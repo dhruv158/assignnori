69e23952976d38c3bf3e90997534325d1f3ba02a
dhruv <dhruvgoel343@gmail.com>
2023-11-09 23:04:01 +0530

    h

---
+++ src/pages/Gas.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ga= (props) => (
+  
+    <li>
+      <img src={props.url} alt="" /> 
+    </li>
+
+);
+
+export default Ga;
---
+++ src/pages/St.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const St= (props) => (
+  <li>{props.title}
+   
+</li>
+);
+
+export default St;
